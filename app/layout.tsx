fe08de627a5eb2df5cfc95d5ab68cad80156ef30
MarLatte <86508134+marlatte@users.noreply.github.com>
2025-02-08 17:29:57 -0500

    Add eslint stylistic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./ui/header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Walker's Work",
@@ -25,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} flex min-h-dvh flex-col antialiased`}
-      >
+      <body className="flex min-h-dvh flex-col antialiased">
         <Header />
         {children}
         <footer className="p-2 text-center">
