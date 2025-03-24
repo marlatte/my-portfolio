f13faf44023179c9a1da225999d4d076236d02e3
MarLatte <86508134+marlatte@users.noreply.github.com>
2025-03-23 21:56:12 -0400

    Add more logos for Tech Stack page

--- app/stack/page.tsx
+++ app/stack/page.tsx
@@ -3,3 +3,30 @@ import ComingSoon from '../../components/ui/coming-soon';
 export default function Page() {
   return <ComingSoon pageName="Tech Stack" />;
 }
+
+/*
+  figma
+  jest
+  next
+  node
+  postgres
+  react
+  typescript
+  javascript
+  react router
+  storybook
+  webpack
+  vite
+  vitest
+  vercel
+  html
+  css
+  tailwind
+  git
+  github
+  azure devops
+  vscode
+  python
+
+
+*/
